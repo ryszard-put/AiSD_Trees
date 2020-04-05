99bfa04568f12c4c410cafeb6254198c05a9d327
Ryszard Poznański <ryszard.poznanski0@gmail.com>
2020-04-05 21:49:43 +0200

    Final?

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -16,6 +16,12 @@ class Controller {
   static DSW_BTN_ID = '#dsw_btn';
   static REMOVE_NODES_BTN_ID = '#remove_nodes_btn';
 
+  // Performance buttons
+  static TEST_INSERT_ID = '#test_insertion';
+  static TEST_INORDER_ID = '#test_inorder';
+  static TEST_MINSEARCH_ID = '#test_minsearch';
+  static TEST_DSW_ID = '#test_dsw';
+
   // Default values
   static MIN_DEFAULT = 'BST min value path: ';
   static MAX_DEFAULT = 'BST max value path: ';
@@ -50,63 +56,95 @@ class Controller {
     this.removeNodesBtn = document.querySelector(
       Controller.REMOVE_NODES_BTN_ID
     );
+    this.testInsertionBtn = document.querySelector(Controller.TEST_INSERT_ID);
+    this.testInorderBtn = document.querySelector(Controller.TEST_INORDER_ID);
+    this.testMinsearchBtn = document.querySelector(
+      Controller.TEST_MINSEARCH_ID
+    );
+    this.testDSWBtn = document.querySelector(Controller.TEST_DSW_ID);
 
     // Add events to control elements
-    this.insertValuesBtn.addEventListener('click', e =>
+    this.insertValuesBtn.addEventListener('click', (e) =>
       this.handleInsertion(e)
     );
-    this.randomFillBtn.addEventListener('click', e => this.randomFill(e));
-    this.minPathBtn.addEventListener('click', e => this.findMinPath(e));
-    this.maxPathBtn.addEventListener('click', e => this.findMaxPath(e));
-    this.inOrderBtn.addEventListener('click', e => this.inOrder(e));
-    this.preOrderBtn.addEventListener('click', e => this.preOrder(e));
-    this.removePostOrderBtn.addEventListener('click', e =>
+    this.randomFillBtn.addEventListener('click', (e) => this.randomFill(e));
+    this.minPathBtn.addEventListener('click', (e) => this.findMinPath(e));
+    this.maxPathBtn.addEventListener('click', (e) => this.findMaxPath(e));
+    this.inOrderBtn.addEventListener('click', (e) => this.inOrder(e));
+    this.preOrderBtn.addEventListener('click', (e) => this.preOrder(e));
+    this.removePostOrderBtn.addEventListener('click', (e) =>
       this.removePostOrder(e)
     );
     // this.drawBtn.addEventListener('click', e => this.drawTreePreOrder(e));
-    this.dswBtn.addEventListener('click', e => this.rebalanceTree(e));
-    this.removeNodesBtn.addEventListener('click', e => this.removeNodes(e));
+    this.dswBtn.addEventListener('click', (e) => this.rebalanceTree(e));
+    this.removeNodesBtn.addEventListener('click', (e) => this.removeNodes(e));
+
+    this.testInsertionBtn.addEventListener('click', (e) =>
+      this.testInsertion(e)
+    );
+    this.testInorderBtn.addEventListener('click', (e) => this.testInorder(e));
+    this.testMinsearchBtn.addEventListener('click', (e) =>
+      this.testMinSearch(e)
+    );
+    this.testDSWBtn.addEventListener('click', (e) => this.testRebalanceBST(e));
     // Share controller with tree
     this.tree.bindController(this);
   }
 }
 
-Controller.prototype.checkPerformance = function(methodName, ...rest) {
+Controller.prototype.checkPerformance = function (methodName, ...rest) {
   if (this.visualize) return alert('Brak testów w trybie prezentacyjnym');
+  console.log('Rozpoczęcie pomiaru czasu');
   let t0 = performance.now();
   this.tree[methodName](...rest);
   let t1 = performance.now();
-  console.log(methodName, `${t1 - t0} ms`);
+  console.log('Zakończenie pomiaru czasu');
+  console.log(`Wynik dla metody: ${methodName}: ${t1 - t0} ms`);
 };
 
-Controller.prototype.testCreateTree = function(amount) {
-  const set = this.generateSet(amount);
-  this.checkPerformance('insertValues', set);
+Controller.prototype.testInsertion = function (e) {
+  e?.preventDefault();
+  this.visualize = false;
+  let amount = parseInt(prompt('Podaj liczbe elementów.'));
+  if (amount > 0) {
+    const set = this.generateSet(amount);
+    this.checkPerformance('insertValues', set);
+  }
+  this.visualize = true;
 };
 
-Controller.prototype.testMinSearch = function() {
+Controller.prototype.testMinSearch = function (e) {
+  e?.preventDefault();
+  this.visualize = false;
   this.checkPerformance('findMin');
+  this.visualize = true;
 };
 
-Controller.prototype.testInorder = function() {
+Controller.prototype.testInorder = function (e) {
+  e?.preventDefault();
+  this.visualize = false;
   this.checkPerformance('inOrderIterative');
+  this.visualize = true;
 };
 
-Controller.prototype.testRebalanceBST = function() {
+Controller.prototype.testRebalanceBST = function (e) {
+  e?.preventDefault();
+  this.visualize = false;
   if (this.type === 'bst') this.checkPerformance('rebalanceDSW', set);
   else alert('Akcja dozwolona tylko dla BST');
+  this.visualize = true;
 };
 
-Controller.prototype.validateInput = function() {
+Controller.prototype.validateInput = function () {
   const { value } = this.inputField;
   let values = value
     .split(' ')
-    .filter(el => !isNaN(el) && el)
-    .map(el => parseInt(el));
+    .filter((el) => !isNaN(el) && el)
+    .map((el) => parseInt(el));
   return values;
 };
 
-Controller.prototype.handleInsertion = function(e) {
+Controller.prototype.handleInsertion = function (e) {
   e.preventDefault();
   const elements = this.validateInput();
   this.inputField.value = '';
@@ -116,7 +154,7 @@ Controller.prototype.handleInsertion = function(e) {
   } else alert('Błędne dane');
 };
 
-Controller.prototype.randomFill = function(e) {
+Controller.prototype.randomFill = function (e) {
   e?.preventDefault();
   let values = [];
   let max = 20,
@@ -133,7 +171,7 @@ Controller.prototype.randomFill = function(e) {
   this.tree.insertValues(values);
 };
 
-Controller.prototype.generateSet = function(amount) {
+Controller.prototype.generateSet = function (amount) {
   let set = [];
   for (let i = amount; i > 0; i--) {
     set.push(i);
@@ -141,7 +179,7 @@ Controller.prototype.generateSet = function(amount) {
   return set;
 };
 
-Controller.prototype.removePostOrder = async function(e) {
+Controller.prototype.removePostOrder = async function (e) {
   e?.preventDefault();
   if (!this.tree.root) return alert('Błąd, drzewo puste');
   this.tree.root = await this.tree.removePostOrder();
@@ -149,55 +187,55 @@ Controller.prototype.removePostOrder = async function(e) {
   this.drawTreePreOrder();
 };
 
-Controller.prototype.resetMinPath = function() {
+Controller.prototype.resetMinPath = function () {
   this.tree.minPathArr = [];
   this.minPathContainer.textContent = Controller.MIN_DEFAULT;
 };
 
-Controller.prototype.findMinPath = function(e) {
+Controller.prototype.findMinPath = function (e) {
   e.preventDefault();
   this.resetMinPath();
   this.tree.findMin();
   this.minPathContainer.textContent += this.tree.minPathArr.join('=>');
 };
 
-Controller.prototype.resetMaxPath = function() {
+Controller.prototype.resetMaxPath = function () {
   this.tree.maxPathArr = [];
   this.maxPathContainer.textContent = Controller.MAX_DEFAULT;
 };
 
-Controller.prototype.findMaxPath = function(e) {
+Controller.prototype.findMaxPath = function (e) {
   e.preventDefault();
   this.resetMaxPath();
   this.tree.findMax();
   this.maxPathContainer.textContent += this.tree.maxPathArr.join('=>');
 };
 
-Controller.prototype.resetInOrder = function() {
+Controller.prototype.resetInOrder = function () {
   this.tree.inOrderArr = [];
   this.inOrderContainer.textContent = Controller.INORDER_DEFAULT;
 };
 
-Controller.prototype.inOrder = function(e) {
+Controller.prototype.inOrder = function (e) {
   e.preventDefault();
   this.resetInOrder();
   this.tree.inOrderIterative();
   this.inOrderContainer.textContent += this.tree.inOrderArr.join(', ');
 };
 
-Controller.prototype.resetPreOrder = function() {
+Controller.prototype.resetPreOrder = function () {
   this.tree.preOrderArr = [];
   this.preOrderContainer.textContent = Controller.PREORDER_DEFAULT;
 };
 
-Controller.prototype.preOrder = function(e) {
+Controller.prototype.preOrder = function (e) {
   e.preventDefault();
   this.resetPreOrder();
   this.tree.preOrder();
   this.preOrderContainer.textContent += this.tree.preOrderArr.join(', ');
 };
 
-Controller.prototype.drawTreePreOrder = async function(e) {
+Controller.prototype.drawTreePreOrder = async function (e) {
   e?.preventDefault();
   if (!this.visualize) return;
   background(36);
@@ -208,19 +246,19 @@ Controller.prototype.drawTreePreOrder = async function(e) {
   }
 };
 
-Controller.prototype.rebalanceTree = function(e) {
+Controller.prototype.rebalanceTree = function (e) {
   e?.preventDefault();
   this.tree.rebalanceDSW();
   this.drawTreePreOrder();
 };
 
-Controller.prototype.removeNodes = async function(e) {
+Controller.prototype.removeNodes = async function (e) {
   e?.preventDefault();
   if (!this.tree.root) return alert('Błąd, drzewo puste');
   let input = prompt('Podaj wartości węzłów do usunięcia');
   if (!input) return alert('Brak węzłów');
 
-  let values = input.split(' ').map(el => parseInt(el));
+  let values = input.split(' ').map((el) => parseInt(el));
 
   for (value of values) {
     const nodeToRemove = this.tree.findNode(value);
